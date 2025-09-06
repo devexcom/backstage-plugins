93f2b2a88d2874c42750b33dff5092014e9a78b7
Naveen Alok <naveenalok@gmail.com>
2025-09-06 15:38:46 +0200

    fix: update ESLint configuration for TypeScript support and adjust ignore patterns

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   root: true,
   extends: ['eslint:recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
@@ -14,10 +16,21 @@ module.exports = {
     es6: true,
     jest: true,
   },
+  ignorePatterns: [
+    'node_modules',
+    'dist',
+    'dist-types',
+    '*.esm.js',
+    '*.cjs.js',
+    '.eslintrc.cjs',
+  ],
   rules: {
     'no-unused-vars': 'off',
     'no-undef': 'off',
     'prefer-const': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
   },
   overrides: [
     {
